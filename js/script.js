f68397a4c6276b62c34ce99d74778efb04d42f2e
mikicola <michela.colangelo@gmail.com>
2022-03-10 16:38:31 +0100

    integro 'hai vinto'

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 const idLevel = document.getElementById('level');
 const btnPlay = document.querySelector('.btn-play');
 const gameArea = document.querySelector('.game-area');
-
-const message = document.querySelector('.message')
-
 let level = document.getElementById('level');
 
+const message = document.querySelector('.message')
+const bombNum = 2; //16; da ripristinare
+
 btnPlay.addEventListener('click', startGame);
-
-
-
 
 
 function startGame() {
     message.innerHTML = '';
     let score = 0; //definisco punteggio iniziale
+    
 
     gameArea.innerHTML = '';
     if (level.value == 'Easy') {
@@ -24,6 +22,7 @@ function startGame() {
             gameArea.append(square);
             square.innerHTML = i;
 
+            totSquares = 100;
             square.addEventListener('click', selectedSquareChangeColor);
             console.log(i)
         }
@@ -35,16 +34,19 @@ function startGame() {
             gameArea.append(square);
             square.innerHTML = i;
 
+            totSquares = 81;
             square.addEventListener('click', selectedSquareChangeColor);
             console.log(i)
         }
     } else if (level.value == 'Hard') {
-        for (let i = 1; i <= 49; i++) {
+        for (let i = 1; i <= 6; i++) {//49; da ripristinare
             let square = document.createElement("div");
             square.classList.add("square-hard");
             gameArea.append(square);
             square.innerHTML = i;
 
+            totSquares = 6;
+            //49; da ripristinare
             square.addEventListener('click', selectedSquareChangeColor);
             console.log(i)
         }
@@ -53,35 +55,40 @@ function startGame() {
 // inizio seconda parte esercizio 
     const arrRandomBombs = [];
 
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < bombNum; i++) {
         let randomNum;
 
         // ciclo per avere 16 numeri random diversi
         do{
-            randomNum = getRandomNum(1, 16);
+            randomNum = getRandomNum(1, totSquares);
         }while(arrRandomBombs.includes(randomNum)){ //per non avere numeri doppo
         arrRandomBombs.push(randomNum)
 
         }
     }
-    console.log(arrRandomBombs)
+    console.log(arrRandomBombs.sort()) //.sort() mette i num in ordire
 
+    const goodSquares = totSquares - bombNum; //definisco le squres belle e gentili
+    // console.log('le buone sono ' + goodSquares)
 
-    function selectedSquareChangeColor(element) {
-        
+    function selectedSquareChangeColor(element) {    
         squareValue = parseInt(this.innerHTML);
         console.log('valore square', squareValue);
-        
-        
-        if (arrRandomBombs.includes(squareValue)){ // = se in arrrandomBombs è incluso il valore di squareValue abbiamo preso una bomba
-            this.classList.add('square-selected-color-bomb'); //cambia colore al click (BOMBA)
-            console.log('hai perso, hai preso una bomba!')
+            
+            if (arrRandomBombs.includes(squareValue)){ // = se in arrrandomBombs è incluso il valore di squareValue abbiamo preso una bomba
+                this.classList.add('square-selected-color-bomb'); //cambia colore al click (BOMBA)
+                console.log('hai perso, hai preso una bomba!')
+    
+                message.innerHTML = 'Hai preso una bomba! Hai totalizzato ' + score + ' punti' 
+            }else{
+                this.classList.add('square-selected-color'); //cambia colore al click
+                score++ //incremento del punteggio
+            }
 
-            message.innerHTML = 'Hai preso una bomba! Hai totalizzato ' + score + ' punti' 
-        }else{
-            this.classList.add('square-selected-color'); //cambia colore al click
-            score++ //incremento del punteggio
-        }
+            if (score == goodSquares){ //se il punteggio è uguale alle good squares hai vinto
+                message.innerHTML = 'Hai vinto! Hai totalizzato ' + score + ' punti' 
+            }
+        
 
         this.removeEventListener('click', selectedSquareChangeColor); //impedisce di ripremere su un elemento square
     }
